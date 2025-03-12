468d2fd7c09090df09f2570252110669f4fef021
TanmayBisht05 <tanmaybisht2005@gmail.com>
2025-03-12 11:37:40 +0530

    sixth commit

--- frontend/src/store/useAuthStore.jsx
+++ frontend/src/store/useAuthStore.jsx
@@ -7,7 +7,6 @@ export const useAuthStore =create((set)=>({
     isSigningUp: false,
     isLoggingIn: false,
     isUpdatingProfile:false,
-
     isCheckingAuth:true,
     
     checkAuth: async()=>{
@@ -88,6 +87,27 @@ export const useAuthStore =create((set)=>({
 
             toast.error(error.response.data.message);
             
+        }
+
+    },
+
+
+    updateProfile: async(data)=>{
+        set({isUpdatingProfile:true});
+
+        try {
+            const res=await axiosInstance.put("/auth/update-profile",data);
+            set({authUser: res.data});
+            toast.success("Profile updated successfully");
+        }
+        catch (error) {
+            console.log("Error in update profile", error);
+            toast.error(error.response.data.message);
+        }
+
+
+        finally{
+            set({isUpdatingProfile:false});
         }
 
     }
