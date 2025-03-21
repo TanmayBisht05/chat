60f807842059fbdc28c2bd5fd1c15e2d7d1276f6
TanmayBisht05 <tanmaybisht2005@gmail.com>
2025-03-21 10:42:24 +0530

    i think this is complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
@@ -12,6 +15,9 @@ dotenv.config();
 // const app=express();               this line is needed any more since we are now importing it from socket.js 
 
 const PORT=process.env.PORT;
+const __dirname=path.resolve();
+
+
 app.use(express.json())
 app.use(cookieParser());
 
@@ -25,6 +31,16 @@ app.use(
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages",messageRoutes);
+
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  });
+
+}
 
 server.listen(PORT, ()=>{
     console.log("server is running on PORT: "+ PORT);
